84c5c1ed7c0b15cd56d00ca44459e1ce6f575544
Piotr Kucharski <kucharski.pt@gmail.com>
2018-11-18 15:39:25 +0100

    Time update

--- game.js
+++ game.js
@@ -263,7 +263,7 @@ function startGame() {
             ctx.shadowColor = "black";
             ctx.shadowBlur = 3;
             ctx.textAlign = "right";
-            ctx.fillText("12:54 18.11.2018", canv.width - 20, 30);
+            ctx.fillText("15:39 18.11.2018", canv.width - 20, 30);
 
             ctx.restore();
 
